df964595ccec72b0187151604a84cb28f8ee4293
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-03-22 19:12:50 -0600

    parte1

---
+++ templates/inicio.php
@@ -0,0 +1,150 @@
+<?php
+    /* Template Name: Inicio */ 
+    get_header();
+?>
+   
+      
+  
+    <section id="banner" class="template-section">
+    <div class="banner2">
+    <h1 class="especial">¿Quieres hacer crecer tu dinero?</h1>
+    <h1>¡Invierte de forma segura en la bolsa! (Desde 500 mil mxp)</h1>
+    <h1>Benefíciate del crecimiento econónomico de empresas como:
+AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h2>
+     <div class="boton"><a href="" class="btn1">ME INTERESA</a>    <a href="" class="btn2">MÁS INFORMACIÓN</a></div> 
+   </div>
+   
+    </section >
+
+ 
+    <section id="inversion" class="template-section">
+          <h2>Si hace 5 años hubiera invertido en acciones de la empresa Apple™ por ejemplo; <br>
+¿Cuál sería mi rendimiento hoy?</h2>
+      <div class="grid">
+           
+                <div class="col">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/invertido.jpg" alt="Invertido">
+               </div>
+                <div class="col quienes">
+                 <p> <strong>Haber ganado + 138%</strong>, es decir, si hubieras invertido 1 millón hace <br>
+5 años hoy tendrías $2,138,000.00 </p>
+        <p>Es importante mencionar que durante esos 5 años llegaste a tener una baja en tu saldo de -59% es decir una baja de $590,000 pero al final tu periodo y objetivo de 5 años el precio de la acción subió pese a las caídas y lograste recuperarte, y no sólo eso, tuviste una ganancia muy rentable:</p>
+        <p class="saldo">¡Un saldo final de $2,138,000.00!</p>
+         <div class="boton"> <a href="http://proservet.mx/quienes-somos/" class="btn1">Me interesa invertir</a></div> 
+               </div>
+     </div>
+        
+    </section>
+        <section id="seguro" class="template-section">
+          <h2>¿Quieres invertir de forma segura y con altos rendimientos?</h2>
+          <p class="inversiones">Las <strong>inversiones en acciones</strong>, son de mediano y largo plazo (mínimo de 3 años para recibir ganancias). <strong>El plazo de inversión ideal es de 5 años en adelante para obtener retornos aún más altos</strong>, con un buen análisis y acompañados de un profesionista que nos ayude. Este tipo de inversiones <strong>son legales</strong> y cubren lo establecido por las leyes mexicanas e internacionales a través de instituciones altamente calificadas-supervisadas-reconocidas que el analista podrá recomendarle. </p>
+      <div class="grid">
+           
+                
+                <div class="col quienes">
+                <h2>NO RECIBIMOS CAPITAL ALGUNO.</h2>
+                 <p class="unicamente">Únicamente <strong> ofrecemos el servicio de asesoría y gestión/administración de su inversión</strong> sin que tengan que depositarnos su dinero a nosotros.</p>
+                 <p class="unicamente">Para su seguridad, <strong>el capital que vaya a invertir lo depositará directamente a una institución financiera</strong> reconocida por el cliente, recomendada por nosotros y que cubre el marco legal correspondiente en sus depósitos y transferencias.</p>
+        
+               </div>
+               <div class="col">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/1019.jpg" alt="Invertido">
+               </div>
+     </div>
+       
+       <p class="gusto">Con gusto <strong> en una cita o llamada personalizada te brindaremos la información con detalle</strong> y ejemplificada.  </p>
+        <div class="boton"><a href="" class="btn1">Contáctanos</a>    </div> 
+</section>
+
+       <section id="beneficio" class="template-section">
+          <h2>BENEFICIOS</h2>
+          <p>inversiones sofisticadas, legales y seguras</p>
+      <div class="grid">
+           
+                <div class="col">
+                  <h3>HAZ CRECER TU CAPITAL</h3>
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/capital.png" alt="Invertido">
+                   <p>Los pequeños y medianos inversionistas pueden participar con capital a partir de 500 mil mxp y beneficiarse del crecimiento económico de  empresas como: Amazon™, Coca Cola™, Google™, LALA™, entre otras.</p>
+               </div>
+                <div class="col">
+                  <h3>INSTITUCIONES CALIFICADAS</h3>
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/bank.png" alt="Invertido">
+                   <p>TU DINERO y Cuenta de Inversión se encuentran exentas de fraude. Las instituciones que recomendamos son las de mayores beneficios para este tipo de inversiones para los clientes (las hemos evaluado) y te brindan seguridad garantizada en tus depósitos y transferencias. </p>
+               </div>
+     </div>
+       <div class="grid segundo">
+           
+                <div class="col">
+                  <h3>INVERSIONES EN BOLSA <br>
+(Acciones México y Acciones EUA)</h3>
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/money.png" alt="Invertido">
+                   <p>Las inversiones en Bolsa o Inversiones en Acciones te dan la posibilidad de comprar acciones de forma legal y eficaz y ser parte del crecimiento de las empresas más representativas y conocidas a nivel mundial. </p>
+               </div>
+                <div class="col">
+                  <h3>CONOCE EL RENDIMIENTO DE TU INVERSIÓN</h3>
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/celular.png" alt="Invertido">
+                   <p>Podrás consultar diariamente a través de una app tu saldo y recibirás estados de cuenta electrónicos y físicos de tu inversión mensualmente y anualmente.</p>
+               </div>
+     </div>
+       <div class="interes">
+       <h3>TASA DE INTERÉS SUPERIOR A LA INFLAC</h3>
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/piggy-bank-with-dollar-coins.png" alt="Invertido">
+                   <p>Las tasas de rentabilidad históricas que han tenido las inversiones en bolsa en plazos de 5 años han sido sumamente superiores a la tasa de inflación de este mismo periodo, por eso, pese a que son inversiones con volatilidad, son también altamente rentables en plazos de inversión de 3 a 5 años.</p>
+        </div>
+        
+          <div class="grid bank">
+           
+                <div class="col">
+                  
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/beneficios.jpg" alt="Invertido">
+                  
+               </div>
+                <div class="col">
+                  <h3>BENEFICIOS DE LA ASESORIA PERSONAL</h3>
+                   <p>TE BRINDAMOS ASESORÍA PERSONAL PARA APERTURA DE CONTRATO:</p>
+                    <p>No te quedarás con dudas.</p>
+                    <p>Obtén asesoría al estructurar una estrategia de inversión: Te recomendamos lo mejor para ti, haciendo un plan realista y totalmente personalizado.
+</p>
+              <p>Se te informa cómo se encuentra el entorno económico: Conocerás el panorama real para que no tengas sorpresas.</p>
+              <p>Contacto permanente contigo: Estarás acompañado en el proceso de inversión.</p>
+               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               </div>
+     </div>
+     <div class="grid bank">
+           
+               
+                <div class="col">
+                  <h3>PLAZO DE INVERSIÓN EN BOLSA DEBE SER DE <br>
+3 A 5 AÑOS:</h3>
+                   <p>El horizonte de inversión para este tipo de planes es de 3 a 5 años o bien superior para obtener una tasa de rendimiento muy rentable y disminuir la volatilidad también.</p>
+                    <p>Podrás disponer de tu Capital si tienes una emergencia. Sólo en un caso extremo comprobable podrías disponer de este capital sin penalizaciones ni comisiones, pero dependiendo del movimiento del mercado y tu saldo actual en este momento.</p>
+                    <p>Históricamente las rentabilidades en plazo de 5 años en bolsa han sido de + 59% para el caso de varias acciones en concreto.</p>
+               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               </div>
+                <div class="col">
+                  
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/61.jpg" alt="Invertido">
+                  
+               </div>
+     </div>
+     
+      <div class="grid bank">
+           
+                <div class="col">
+                  
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/consulta.jpg" alt="Invertido">
+                  
+               </div>
+                <div class="col">
+                  <h3>CONSULTA TU INVERSIÓN DESDE INTERNET</h3>
+                   <p>Te daremos acceso a una banca electrónica en Internet para que puedas consultar el estado de tu inversión en cualquier momento.</p>
+               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               </div>
+     </div>
+     
+    </section>
+  <?php include 'contacto.php'; ?>
+ 
+<?php 
+    get_footer();
+?>

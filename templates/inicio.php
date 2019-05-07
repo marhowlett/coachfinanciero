779c1a08dd1f0b9413d2d06007c1041ef7cb05fc
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-05-07 14:56:14 -0500

    .

--- templates/inicio.php
+++ templates/inicio.php
@@ -12,7 +12,7 @@
     <h1>¡Invierte de forma segura en la bolsa! (Desde 500 mil mxp)</h1>
     <h1>Benefíciate del crecimiento econónomico de empresas como:
 AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
-     <div class="boton"><a href="" class="btn1">ME INTERESA</a>    <a href="" class="btn2">MÁS INFORMACIÓN</a></div> 
+     <div class="boton"><a href="#contacto" class="btn1">ME INTERESA</a>    <a href="#seguro" class="btn2">MÁS INFORMACIÓN</a></div> 
    </div>
        
     </section >
@@ -23,17 +23,17 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
           <h2>Si hace 5 años hubiera invertido en acciones de la empresa Apple™ por ejemplo; <br>
 ¿Cuál sería mi rendimiento hoy?</h2>
     
-      <div class="grid">
+      <div class="grid ">
            
                 <div class="col">
-                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/invertido.jpg" alt="Invertido">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/estadisticas.png" alt="Invertido">
                </div>
                 <div class="col quienes">
                  <p> <strong>Haber ganado + 138%</strong>, es decir, si hubieras invertido 1 millón hace <br>
 5 años hoy tendrías $2,138,000.00 </p>
         <p>Es importante mencionar que durante esos 5 años llegaste a tener una baja en tu saldo de -59% es decir una baja de $590,000 pero al final tu periodo y objetivo de 5 años el precio de la acción subió pese a las caídas y lograste recuperarte, y no sólo eso, tuviste una ganancia muy rentable:</p>
         <p class="saldo">¡Un saldo final de $2,138,000.00!</p>
-         <div class="boton"> <a href="http://proservet.mx/quienes-somos/" class="btn1">Me interesa invertir</a></div> 
+         <div class="boton"> <a href="#contacto" class="btn1">Me interesa invertir</a></div> 
                </div>
      </div>
         </div>
@@ -42,7 +42,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
            <div class="contenedor">
           <h2>¿Quieres invertir de forma segura y con altos rendimientos?</h2>
           <p class="inversiones">Las <strong>inversiones en acciones</strong>, son de mediano y largo plazo (mínimo de 3 años para recibir ganancias). <strong>El plazo de inversión ideal es de 5 años en adelante para obtener retornos aún más altos</strong>, con un buen análisis y acompañados de un profesionista que nos ayude. Este tipo de inversiones <strong>son legales</strong> y cubren lo establecido por las leyes mexicanas e internacionales a través de instituciones altamente calificadas-supervisadas-reconocidas que el analista podrá recomendarle. </p>
-      <div class="grid">
+      <div class="grid invertido">
            
                 
                 <div class="col quienes">
@@ -57,7 +57,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
      </div>
        
        <p class="gusto">Con gusto <strong> en una cita o llamada personalizada te brindaremos la información con detalle</strong> y ejemplificada.  </p>
-        <div class="boton"><a href="" class="btn1">Contáctanos</a>    </div> 
+        <div class="boton"><a href="#contacto" class="btn1">Contáctanos</a>    </div> 
             </div>
 </section>
 
@@ -78,7 +78,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
                    <p>TU DINERO y Cuenta de Inversión se encuentran exentas de fraude. Las instituciones que recomendamos son las de mayores beneficios para este tipo de inversiones para los clientes (las hemos evaluado) y te brindan seguridad garantizada en tus depósitos y transferencias. </p>
                </div>
      </div>
-       <div class="grid segundo">
+       <div class="grid segundo invertido">
            
                 <div class="col">
                   <h3>INVERSIONES EN BOLSA <br>
@@ -95,7 +95,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
        <div class="interes">
        <h3>TASA DE INTERÉS SUPERIOR A LA INFLACIÓN</h3>
                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/piggy-bank-with-dollar-coins.png" alt="Invertido">
-                   <p>Las tasas de rentabilidad históricas que han tenido las inversiones en bolsa en plazos de 5 años han sido sumamente superiores a la tasa de inflación de este mismo periodo, por eso, pese a que son inversiones con volatilidad, son también altamente rentables en plazos de inversión de 3 a 5 años.</p>
+                   <p class="tasas">Las tasas de rentabilidad históricas que han tenido las inversiones en bolsa en plazos de 5 años han sido sumamente superiores a la tasa de inflación de este mismo periodo, por eso, pese a que son inversiones con volatilidad, son también altamente rentables en plazos de inversión de 3 a 5 años.</p>
         </div>
         
           <div class="grid bank">
@@ -113,10 +113,10 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
 </p>
               <p>Se te informa cómo se encuentra el entorno económico: Conocerás el panorama real para que no tengas sorpresas.</p>
               <p>Contacto permanente contigo: Estarás acompañado en el proceso de inversión.</p>
-               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               <div class="boton"><a href="#contacto" class="btn1">Me interesa</a>    </div> 
                </div>
      </div>
-     <div class="grid bank">
+     <div class="grid bank invertido">
            
                
                 <div class="col">
@@ -125,7 +125,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
                    <p>El horizonte de inversión para este tipo de planes es de 3 a 5 años o bien superior para obtener una tasa de rendimiento muy rentable y disminuir la volatilidad también.</p>
                     <p>Podrás disponer de tu Capital si tienes una emergencia. Sólo en un caso extremo comprobable podrías disponer de este capital sin penalizaciones ni comisiones, pero dependiendo del movimiento del mercado y tu saldo actual en este momento.</p>
                     <p>Históricamente las rentabilidades en plazo de 5 años en bolsa han sido de + 59% para el caso de varias acciones en concreto.</p>
-               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               <div class="boton"><a href="#contacto" class="btn1">Me interesa</a>    </div> 
                </div>
                 <div class="col">
                   
@@ -144,7 +144,7 @@ AmazonTM , Coca ColaTM  y BIMBOTM , entre otras.</h1>
                 <div class="col">
                   <h3>CONSULTA TU INVERSIÓN DESDE INTERNET</h3>
                    <p>Te daremos acceso a una banca electrónica en Internet para que puedas consultar el estado de tu inversión en cualquier momento.</p>
-               <div class="boton"><a href="" class="btn1">Me interesa</a>    </div> 
+               <div class="boton"><a href="#contacto" class="btn1">Me interesa</a>    </div> 
                </div>
      </div>
            </div>
